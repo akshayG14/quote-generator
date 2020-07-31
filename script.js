ac58e8f25a108a8efbd9b9a24d814efc0f21fe59
AkshayG <akshayg14@gmail.com>
2020-07-31 14:19:26 +0530

    Added quote fetch functionality.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("JS Loaded!!!");
+
+// Get the quote from API
+async function getQuote() {
+  const cors_api_host = "cors-anywhere.herokuapp.com";
+  const cors_api_url = "https://" + cors_api_host + "/";
+  const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+
+  try {
+    const response = await fetch(cors_api_url + apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("Sorry but no quote", error);
+  }
+}
+
+// on load
+getQuote();
