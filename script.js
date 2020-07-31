8ac2b7490209f73e1dad15aebcb4a891b5ac158c
AkshayG <akshayg14@gmail.com>
2020-07-31 14:48:17 +0530

    Added proxy server.
    Added proxy server to fix the CORS error.

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 console.log("JS Loaded!!!");
 
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBttn = document.getElementById("twitter-button");
+const newQuoteBttn = document.getElementById("new-quote-button");
+
 // Get the quote from API
 async function getQuote() {
-  const cors_api_host = "cors-anywhere.herokuapp.com";
-  const cors_api_url = "https://" + cors_api_host + "/";
+  const proxyUrl = "https://frozen-lake-72991.herokuapp.com/";
   const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
 
   try {
-    const response = await fetch(cors_api_url + apiUrl);
+    const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+    authorText.innerText = data.quoteAuthor;
+    quoteText.innerText = data.quoteText;
   } catch (error) {
-    console.log("Sorry but no quote", error);
+    getQuote();
+    // console.log("Sorry but no quote", error);
   }
 }
 
